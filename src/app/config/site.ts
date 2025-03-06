db6d4af4b6c304d75d4efeb60fdc9b4f5ff624b1
Sanmi Akinwunmi <sanmi.hq@gmail.com>
2025-03-06 21:58:29 +0100

    chore: app configuration

---
+++ src/app/config/site.ts
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: "LinkTrackr",
+  title: "LinkTrackr - Smart Link Logging",
+  url: "https://ui.shadcn.com",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description:
+    "Generate trackable links to log visitor data before redirecting to the original URL.",
+  links: {
+    x: "https://x.com/sanmi_hq",
+    github: "https://github.com/sanmihq/linktrackr",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
