5850e69fbcd9457c6ae4a3d950aa88b01d959519
Sanmi Akinwunmi <sanmi.hq@gmail.com>
2025-03-07 21:05:49 +0100

    feat: integrate Clerk for authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,19 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { geistSans } from "./fonts/fonts";
 import { siteConfig } from "./config/site";
+import { Header } from "./(app)/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: siteConfig.title,
   description: siteConfig.description,
 };
+
+const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.className} antialiased`}>{children}</body>
+      <body className={`${geistSans.className} antialiased`}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <Header />
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
